4fcbe7b9940d3a5fc3cef3166d3f069114d788aa
Zepyyy <quentin.stubecki@viacesi.fr>
2025-02-02 10:41:55 +0100

    smaller icon + indent

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,42 @@ import { Button } from "./components/ui/button";
 import { MoonStar, SunMedium } from "lucide-react";
 
 export default function App() {
-    const [theme, setTheme] = useState<string>("light");
-    document.documentElement.classList.toggle("dark", theme === "dark");
+	const [theme, setTheme] = useState<string>("light");
+	document.documentElement.classList.toggle("dark", theme === "dark");
 
-    return (
-        <ThemeContext.Provider value={theme}>
-            <main className="flex min-h-screen bg-background text-foreground selection:bg-primary selection:text-primary-foreground">
-                <div className="flex flex-col justify-start w-full p-24">
-                    <Input
-                        type="text"
-                        placeholder="Enter tab name"
-                        className="font-bold mb-4 border-none shadow-none focus-visible:ring-0 w-1/6 text-3xl md:text-4xl py-0 h-fit"
-                    />
-                    <GuitarTabCreator />
+	return (
+		<ThemeContext.Provider value={theme}>
+			<main className="flex min-h-screen bg-background text-foreground selection:bg-primary selection:text-primary-foreground">
+				<div className="flex flex-col justify-start w-full p-24">
+					<Input
+						type="text"
+						placeholder="Enter tab name"
+						className="font-bold mb-4 border-none shadow-none focus-visible:ring-0 w-1/6 text-3xl md:text-4xl py-0 h-fit"
+					/>
+					<GuitarTabCreator />
 
-                    <div className="flex justify-between items-center">
-                        <div>
-                            <p className="text-sm text-tab">
-                                Click to increment fret number
-                            </p>
-                            <p className="text-sm text-tab">
-                                Right-click to switch note open/mute/off
-                            </p>
-                        </div>
-                    </div>
-                    <Button
-                        variant={"outline"}
-                        size="icon"
-                        className="[&_svg]:size-10 border-none w-min h-min hover:text-primary hover:bg-primary-foreground absolute top-4 right-4"
-                        onClick={() =>
-                            setTheme(theme === "light" ? "dark" : "light")
-                        }
-                    >
-                        {theme === "light" ? <MoonStar /> : <SunMedium />}
-                    </Button>
-                </div>
-            </main>
-        </ThemeContext.Provider>
-    );
+					<div className="flex justify-between items-center">
+						<div>
+							<p className="text-sm text-tab">
+								Click to increment fret number
+							</p>
+							<p className="text-sm text-tab">
+								Right-click to switch note open/mute/off
+							</p>
+						</div>
+					</div>
+					<Button
+						variant={"outline"}
+						size="icon"
+						className="[&_svg]:size-8 border-none w-min h-min hover:text-primary hover:bg-primary-foreground absolute top-4 right-4"
+						onClick={() =>
+							setTheme(theme === "light" ? "dark" : "light")
+						}
+					>
+						{theme === "light" ? <MoonStar /> : <SunMedium />}
+					</Button>
+				</div>
+			</main>
+		</ThemeContext.Provider>
+	);
 }
--- src/guitar-tab-creator.tsx
+++ src/guitar-tab-creator.tsx
@@ -9,124 +9,129 @@ const MUTED_STRING = "X";
 const MAX_COLS = 48;
 
 export default function GuitarTabCreator() {
-    const [NOTES, setNOTES] = useState(24);
+	const [NOTES, setNOTES] = useState(24);
 
-    const [tab, setTab] = useState<string[][]>(
-        Array(STRINGS)
-            .fill(null)
-            .map(() => Array(NOTES).fill(DEFAULT_NOTE))
-    );
+	const [tab, setTab] = useState<string[][]>(
+		Array(STRINGS)
+			.fill(null)
+			.map(() => Array(NOTES).fill(DEFAULT_NOTE))
+	);
 
-    const handleCellClick = (string: number, note: number) => {
-        const newTab = tab.map((row, i) =>
-            row.map((cell, j) =>
-                i === string && j === note
-                    ? cell === DEFAULT_NOTE
-                        ? OPEN_STRING
-                        : cell === OPEN_STRING
-                        ? MUTED_STRING
-                        : DEFAULT_NOTE
-                    : cell
-            )
-        );
-        setTab(newTab);
-        console.log(string);
-    };
+	const handleCellClick = (string: number, note: number) => {
+		const newTab = tab.map((row, i) =>
+			row.map((cell, j) =>
+				i === string && j === note
+					? cell === DEFAULT_NOTE
+						? OPEN_STRING
+						: cell === OPEN_STRING
+						? MUTED_STRING
+						: DEFAULT_NOTE
+					: cell
+			)
+		);
+		setTab(newTab);
+		console.log(string);
+	};
 
-    const incrementNotesNumber = (string: number, note: number) => {
-        const newTab = [...tab];
-        const currentValue = newTab[string][note];
-        switch (true) {
-            case currentValue === DEFAULT_NOTE:
-                newTab[string][note] = "1";
-                break;
-            case currentValue === MUTED_STRING:
-                newTab[string][note] = DEFAULT_NOTE;
-                break;
-            default: {
-                const nextValue = Number.parseInt(currentValue) + 1;
-                newTab[string][note] =
-                    nextValue > 24 ? DEFAULT_NOTE : nextValue.toString();
-                break;
-            }
-        }
-        setTab(newTab);
-    };
+	const incrementNotesNumber = (string: number, note: number) => {
+		const newTab = [...tab];
+		const currentValue = newTab[string][note];
+		switch (true) {
+			case currentValue === DEFAULT_NOTE:
+				newTab[string][note] = "1";
+				break;
+			case currentValue === MUTED_STRING:
+				newTab[string][note] = DEFAULT_NOTE;
+				break;
+			default: {
+				const nextValue = Number.parseInt(currentValue) + 1;
+				newTab[string][note] =
+					nextValue > 24 ? DEFAULT_NOTE : nextValue.toString();
+				break;
+			}
+		}
+		setTab(newTab);
+	};
 
-    const clearTab = () => {
-        setTab(
-            Array(STRINGS)
-                .fill(null)
-                .map(() => Array(NOTES).fill(DEFAULT_NOTE))
-        );
-    };
+	const clearTab = () => {
+		setTab(
+			Array(STRINGS)
+				.fill(null)
+				.map(() => Array(NOTES).fill(DEFAULT_NOTE))
+		);
+	};
 
-    const handleAddNotesClick = (amount: number = 1) => {
-        switch (true) {
-            case NOTES + amount > MAX_COLS:
-                amount = MAX_COLS - NOTES;
-                break;
-            case NOTES + amount < 1:
-                amount = 1;
-                break;
-        }
-        setNOTES(NOTES + amount);
-        setTab((prevTab) => {
-            return prevTab.map((row) => [
-                ...row,
-                ...Array(amount).fill(DEFAULT_NOTE),
-            ]);
-        });
-    };
+	const handleAddNotesClick = (amount: number = 1) => {
+		switch (true) {
+			case NOTES + amount > MAX_COLS:
+				amount = MAX_COLS - NOTES;
+				break;
+			case NOTES + amount < 1:
+				amount = 1;
+				break;
+		}
+		setNOTES(NOTES + amount);
+		setTab((prevTab) => {
+			return prevTab.map((row) => [
+				...row,
+				...Array(amount).fill(DEFAULT_NOTE),
+			]);
+		});
+	};
 
-    return (
-        <div className="container p-4">
-            <div className="mb-4 flex flex-row items-center">
-                <div className="rounded-md">
-                    {tab.map((string, i) => (
-                        <div key={i} className="flex" id="row">
-                            {string.map((note, j) => (
-                                <div
-                                    key={j}
-                                    className="border-r-2 last:border-none [&:nth-child(6n)]:border-tab border-tabsubtle w-8 h-8 flex items-center justify-center cursor-grabbing text-foreground z-10"
-                                    onClick={() => incrementNotesNumber(i, j)}
-                                    onContextMenu={(e) => {
-                                        e.preventDefault();
-                                        handleCellClick(i, j);
-                                    }}
-                                    id="note"
-                                    data-value={note}
-                                >
-                                    {note}
-                                </div>
-                            ))}
-                        </div>
-                    ))}
-                </div>
-                <Button
-                    variant={"default"}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        handleAddNotesClick(6);
-                    }}
-                    className="w-8 h-full ml-2 py-12 hover:bg-primary hover:tesxt-primary-foreground"
-                >
-                    +
-                </Button>
-            </div>
-            <div className="flex gap-4">
-                <Button variant={"default"} onClick={clearTab}>
-                    Clear Tab
-                </Button>
-                <Button
-                    variant={"secondary"}
-                    onClick={() => {
-                        console.log("Yo export?");
-                    }}
-                >
-                    Export?
-                </Button>
-            </div>
-        </div>
-    );
+	return (
+		<div className="container p-4">
+			<div className="mb-4 flex flex-row items-center">
+				<div className="rounded-md">
+					{tab.map((string, i) => (
+						<div key={i} className="flex" id="row">
+							{string.map((note, j) => (
+								<div
+									key={j}
+									className="border-r-2 last:border-none [&:nth-child(6n)]:border-tab border-tabsubtle w-8 h-8 flex items-center justify-center cursor-grabbing text-foreground z-10"
+									onClick={() => incrementNotesNumber(i, j)}
+									onContextMenu={(e) => {
+										e.preventDefault();
+										handleCellClick(i, j);
+									}}
+									id="note"
+									data-value={note}
+								>
+									{note}
+								</div>
+							))}
+						</div>
+					))}
+				</div>
+				<Button
+					variant={"default"}
+					onClick={(e) => {
+						e.preventDefault();
+						handleAddNotesClick(6);
+					}}
+					className="w-8 h-full ml-2 py-12 hover:bg-primary hover:tesxt-primary-foreground"
+				>
+					+
+				</Button>
+			</div>
+			<div className="flex gap-4">
+				<Button
+					variant={"default"}
+					onClick={clearTab}
+					className="hover:opacity-50 hover:bg-primary hover:text-primary-foreground"
+				>
+					Clear Tab
+				</Button>
+				<Button
+					variant={"secondary"}
+					className="hover:opacity-50 hover:bg-secondary hover:text-secondary-foreground"
+					onClick={() => {
+						console.log("Yo export?");
+					}}
+				>
+					Export?
+				</Button>
+			</div>
+		</div>
+	);
 }
