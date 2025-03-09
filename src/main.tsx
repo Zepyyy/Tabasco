6c9d19f87077d76957160f99ac1e0918195b9485
Zepyyy <quentin.stubecki@viacesi.fr>
2025-03-09 20:33:37 +0100

    better

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="*" />
-				<Route path="/:tabId" element={<App />} />
+				<Route path="*" element={<Navigate to="/sheet/0" replace />} />
+				<Route path="/sheet">
+					<Route path="/sheet/:tabId" element={<App />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
-	</StrictMode>,
+	</StrictMode>
 );
